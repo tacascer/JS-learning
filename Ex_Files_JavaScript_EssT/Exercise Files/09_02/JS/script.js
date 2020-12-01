8a7a96d001e6c98418b4bea9124c73f726b7345d
tacascer <trandangtrithanh2000@gmail.com>
2020-12-01 12:31:07 -0500

    Learned about loops

--- Ex_Files_JavaScript_EssT/Exercise Files/09_02/JS/script.js
+++ Ex_Files_JavaScript_EssT/Exercise Files/09_02/JS/script.js
@@ -1,1 +1,8 @@
+//Any href that starts with http.
+var extLinks = document.querySelectorAll('a[href^="http"]')
 
+for (var i = 0; i < extLinks.length; i++) {
+  if (!extLinks[i].hasAttribute("target")) {
+    extLinks[i].setAttribute("target", "_blank")
+  }
+}
